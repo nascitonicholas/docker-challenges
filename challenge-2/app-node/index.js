7e585d6daca731edb43423d8c1d1b50d85d30692
Nicholas Nascimento <nascito.nicholas@gmail.com>
2023-05-16 23:20:10 -0300

    chore: creating Nginx as a reverse proxy and a Node.js service that inserts and retrieves records from MySQL.

--- challenge-2/app-node/index.js
+++ challenge-2/app-node/index.js
@@ -1,9 +1,40 @@
+const { error } = require('console')
 const express = require('express')
 const app = express()
 const port = 3000
+const config = {
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb'
+}
+const mysql = require('mysql')
+const tablePeople = 'people'
+const check = `SELECT 1 FROM ${tablePeople} LIMIT 1`
+const insert = `INSERT INTO people(NAME) VALUES('NICHOLAS')`
+const select = `SELECT * FROM people`
 
 app.get('/', (req, res) => {
-  res.send('<h1>Full Cycle</h1>')
+  const connection = mysql.createConnection(config);
+
+  connection.query(insert);
+
+  connection.query(select, (error, results, fields) => {
+    if (error) {
+      console.error('Erro ao executar a consulta: ', error);
+      return;
+    }
+
+    console.log('Resultado da consulta: ', results);
+    const rows = results.map(row => row.name);
+    const rowsString = rows.join(', ');
+
+    console.log('Metadados dos campos: ', fields);
+
+    res.send('<div><h1>Full Cycle</h1><ol>' + rowsString + '</ol></div>')
+  })
+
+  connection.end()
 })
 
 app.listen(port, () => {
